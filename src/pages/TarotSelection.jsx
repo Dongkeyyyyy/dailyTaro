2c5783fe7ca6d0427bdf7d83331ad6138ffc8dd1
Dongkeyyyyy <yehhhh2@gmail.com>
2025-01-23 17:52:57 +0900

    카드 배치 레이아웃 수정

--- src/pages/TarotSelection.jsx
+++ src/pages/TarotSelection.jsx
@@ -1,12 +1,19 @@
 import Card from "../components/Card";
+import "./TarotSelection.css";
+import Button from "../components/Button";
 
 const TarotSelection = () => {
   const cards = Array(22).fill({ Card });
   return (
-    <div className="cardsWrapper">
-      {cards.map((card, index) => (
-        <Card key={index} />
-      ))}
+    <div className="TarotSelection">
+      <div className="cardsWrapper">
+        {cards.map((card, index) => (
+          <Card key={index} />
+        ))}
+      </div>
+      <div className="button_section">
+        <Button />
+      </div>
     </div>
   );
 };
