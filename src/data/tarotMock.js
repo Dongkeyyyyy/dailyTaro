7a007f1b9210fea1f2fe3aa7c3fd00a581bf843e
donkey <a@b.com>
2025-01-26 17:15:18 +0900

    카드섞기 함수 완료료

---
+++ src/data/tarotMock.js
@@ -0,0 +1,22 @@
+const tarotMock = [
+  {
+    id: 1,
+    name: "Card 1",
+    image: "./assets/cardImages/00theFool.jpg", // public 폴더 기준 경로
+    meaning_up: "새로운 시작",
+  },
+  {
+    id: 2,
+    name: "Card 2",
+    image: "./assets/cardImages/01theMagician.jpg",
+    meaning_up: "잠재력",
+  },
+  {
+    id: 3,
+    name: "Card 3",
+    image: "./assets/cardImages/02priestess.jpg",
+    meaning_up: "직관",
+  },
+];
+
+export default tarotMock;
--- src/pages/PickTarot.jsx
+++ src/pages/PickTarot.jsx
@@ -1,13 +1,26 @@
 import Card from "../components/Card";
+import tarotCardBack from "../assets/tarotCardBack.jpg";
 import "./PickTarot.css";
 import Button from "../components/Button";
+import tarotMock from "../data/tarotMock";
+import { useState } from "react";
 
 const PickTarot = () => {
-  const cards = Array(3).fill({ Card });
+  const [shuffledCards, setShuffledCards] = useState([]); // 섞이면 컴포넌트를 다시 렌더링함
+  const cards = Array(3)
+    .fill(null)
+    .map((_, index) => ({
+      id: index + 1, // 고유 ID (1부터 시작)
+      name: `Card ${index + 1}`, // 카드 이름
+      image: "./assets/tarotCardBack.jpg", // 카드 뒷면 이미지 경로
+    }));
+
+  console.log(cards);
+  console.log("목데이터 가져와", tarotMock);
 
   // 카드 셔플 랜덤 함수
   const shuffleCards = (cards) => {
-    let shuffledCards = [cards]; // 일단 왜 복제하는지 모름
+    let shuffledCards = [...cards];
     //Fisher-Yates 알고리즘 사용하기
     for (let i = shuffledCards.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1)); // 0부터 i 사이의 랜덤 인덱스 -> i + 1 해야함
@@ -18,19 +31,24 @@ const PickTarot = () => {
     }
     console.log("섞기전:", cards);
     console.log("섞기후:", shuffledCards);
-    return shuffledCards;
+    setShuffledCards(shuffledCards); // 섞은 카드 상태 업데이트
   };
   // 카드 섞기 이벤트
   const onClickShuffleCards = () => {
     // 카드 셔플 랜덤 함수 호출
-    shuffleCards();
+    shuffleCards(cards); // cards 배열을 넘겨서 shuffle
   };
   return (
     <div className="PickTarot">
       <div className="cardsWrapper">
-        {cards.map((card, index) => (
-          <Card key={index} />
-        ))}
+        {/* 섞은 카드가 있으면 렌더링, 아니면 원본카드 */}
+        {shuffledCards.length > 0
+          ? shuffledCards.map((card) => (
+              <img key={card.id} src={tarotCardBack} alt={card.name} />
+            ))
+          : cards.map((card) => (
+              <img key={card.id} src={tarotCardBack} alt={card.name} />
+            ))}
       </div>
       <div className="button_section">
         <Button
